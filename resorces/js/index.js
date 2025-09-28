77a9b8b1618552c4f7755e2d9b2a271d48e411cc
Michele Modica <mchlmdc98@gmail.com>
2025-09-28 11:15:21 +0200

    sistemazione navbar css and js

--- resorces/js/index.js
+++ resorces/js/index.js
@@ -1,11 +1,18 @@
-getMainSelection();
 document.addEventListener('DOMContentLoaded', (event) => {
+    let  mainSelection = getMainSelection();
     const iconsNavBar = document.querySelectorAll('.navbar-icon');
     iconsNavBar.forEach(icon =>{
         icon.addEventListener('click', function(){
             const id = this.id;
+            const icons = document.querySelectorAll('.navbar-icon');
+            icons.forEach(el => {
+                el.classList.remove('selected');
+            });
+            icon.classList.add('selected');
             setMainSelection(id);
-            getMainSelection();
+            mainSelection = getMainSelection();
         });
     });
+    const idMainSelection = mainSelection.replace(".html","");
+    document.getElementById(idMainSelection).classList.add('selected');
 });
--- resorces/js/main.js
+++ resorces/js/main.js
@@ -1,10 +1,12 @@
 function getMainSelection(){
+    const container = document.querySelector('main');
     let mainSelection = localStorage.getItem("mainSelection");
     if(!mainSelection){
         localStorage.setItem("mainSelection","today.html");
     }
     mainSelection = localStorage.getItem("mainSelection");
-    fetch(mainSelection).then(response => response.text()).then(date => {document.querySelector('main').innerHTML = date});
+    fetch(mainSelection).then(response => response.text()).then(date => {container.innerHTML = date});
+    return mainSelection;
 }
 function setMainSelection(nameFile){
     localStorage.setItem("mainSelection",nameFile+".html");
