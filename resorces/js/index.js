526dd8e0f96f0c9a22be2dd85907dd520e55912c
Michele Modica <mchlmdc98@gmail.com>
2025-09-28 17:34:58 +0200

    modifica css e js per animazioni

--- resorces/js/index.js
+++ resorces/js/index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let  mainSelection = getMainSelection();
-    const iconsNavBar = document.querySelectorAll('.navbar-icon');
+    const iconsNavBar = document.querySelectorAll('.navbar-icon-main-page');
     iconsNavBar.forEach(icon =>{
         icon.addEventListener('click', function(){
             const id = this.id;
-            const icons = document.querySelectorAll('.navbar-icon');
+            const icons = document.querySelectorAll('.navbar-icon-main-page');
             icons.forEach(el => {
                 el.classList.remove('selected');
             });
@@ -15,4 +15,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
     const idMainSelection = mainSelection.replace(".html","");
     document.getElementById(idMainSelection).classList.add('selected');
+    const addTask = document.getElementById('addTask');
+    const svgIcon = addTask.querySelector('svg');
+    const test = document.getElementById('test');
+    addTask.addEventListener('click', () => {
+        svgIcon.classList.toggle('rotate-45');
+        test.classList.toggle('hide');
+    });
+
 });
