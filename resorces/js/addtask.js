c95ae579543d12ed119d1748f7f941120d3d4942
Michele Modica <mchlmdc98@gmail.com>
2025-10-13 03:55:30 +0200

    aggiunta effetto blur form, aggiunta margin alle incone della navbar

--- resorces/js/addtask.js
+++ resorces/js/addtask.js
@@ -2,16 +2,44 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputTask = document.querySelectorAll('.task');
     const inputNote = document.querySelector('.input-group-note');
     const select = document.getElementById('genere');
-    function aggiornaVisibilita(){
+
+    const taskGiornaliera = document.getElementById('taskGiornaliera');
+    const inputToday = document.querySelectorAll('.task-today');
+    const inputPriority = document.getElementById('input-priority');
+
+    const inGiornata = document.getElementById('inGiornata');
+    const endTask = document.querySelectorAll('.end-task');
+
+    function aggiornaVisibilita() {
         const valore = select.value;
-        if(valore === 'note'){
-            inputTask.forEach(el=>{el.classList.add('hide');});
-            inputNote.classList.remove('hide');            
-        }else{
-            inputTask.forEach(el=>{el.classList.remove('hide');});
+
+        if(valore === 'note') {
+            inputTask.forEach(el => el.classList.add('hide'));
+            inputNote.classList.remove('hide');
+        } else {
+            inputTask.forEach(el => el.classList.remove('hide'));
             inputNote.classList.add('hide');
+
+            if(taskGiornaliera.checked) {
+                inputToday.forEach(el => el.classList.add('hide'));
+                endTask.forEach(el => el.classList.add('hide'));
+                inputPriority.classList.remove('hide');
+            } else {
+                inputToday.forEach(el => el.classList.remove('hide'));
+                inputPriority.classList.add('hide');
+
+                if(inGiornata.checked) {
+                    endTask.forEach(el => el.classList.add('hide'));
+                } else {
+                    endTask.forEach(el => el.classList.remove('hide'));
+                }
+            }
         }
     }
-    aggiornaVisibilita();
+
+    taskGiornaliera.addEventListener('change', aggiornaVisibilita);
+    inGiornata.addEventListener('change', aggiornaVisibilita);
     select.addEventListener('change', aggiornaVisibilita);
+
+    aggiornaVisibilita();
 });
